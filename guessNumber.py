ef0c66c8e901d5f223cc53abecb1d9176964fbe7
FatemeZamanian <fatemehzamanian.1999@gmail.com>
2021-04-28 03:23:25 +0430

    AS10

--- guessNumber.py
+++ guessNumber.py
@@ -1,28 +1,43 @@
 import telebot
 import random
 global c,r
-c=0
+c=5
 bot=telebot.TeleBot('1776106722:AAESboQEm2OMFXjOe9G-z3Qbn8hKRpxTEpA')
+btns=telebot.types.ReplyKeyboardMarkup(row_width=1)
+btn1=telebot.types.KeyboardButton('شروع بازي جديد😁')
+btns.add(btn1)
+def new_game():
+    global r,c
+    c=5
+    r=random.randint(0,30)
+
 @bot.message_handler(commands=['start'])
 def wlc(message):
-    global r
-    bot.send_message(message.chat.id,'سلام خوش اومدي😍 يك عدد بين 1 تا 30 حدس بزن🤷‍♂️')
-    r=random.randint(0,30)
+    bot.send_message(message.chat.id,'سلام خوش اومدي شروع كنيم؟😍',reply_markup=btns)
 @bot.message_handler(func=lambda message:True)
 def guess(message):
-    
-    global r
-    # bot.send_message(message.chat.id,str(r))
-    mr=int(message.text)
-    #c+=1
-    if r==mr:
-        bot.send_message(message.chat.id,'🎉آفرين برنده شدي ')
-        bot.send_message(message.chat.id,'اول استارت بزن بعد عدد جديد رو حدس بزن✌ /start ')
-        
-        #bot.send_message(message.chat.id,'تعداد حدس ها::'+str(c))
-    elif r>mr:
-       bot.send_message(message.chat.id,'عدد بيشتري وارد كن👆')
-    elif r<mr:
-            bot.send_message(message.chat.id,'عدد كمتري وارد كن👇')
+    global r,c
+    if message.text=='شروع بازي جديد😁':
+        bot.send_message(message.chat.id,'عدد جديد رو حدس بزن')
+        new_game()
+    else:
+        mr=int(message.text)
+        c-=1
+        if c>0:
+            if r==mr:
+                bot.send_message(message.chat.id,'🎉آفرين برنده شدي ')
+                bot.send_message(message.chat.id,'عدد جديد رو حدس بزن')
+                new_game()
+            elif r>mr:
+                bot.send_message(message.chat.id,'عدد بيشتري وارد كن👆')
+                bot.send_message(message.chat.id,'تعداد حدس هاي باقي مونده:'+str(c))
+            elif r<mr:
+                bot.send_message(message.chat.id,'عدد كمتري وارد كن👇')
+
+                bot.send_message(message.chat.id,'تعداد حدس هاي باقي مونده:'+str(c))
+        else:
+            bot.send_message(message.chat.id,'باختي كه😒يه بازي ديگه شروع كن')
+            bot.send_message(message.chat.id,'عدد جديد رو حدس بزن')
+            new_game()
 
 bot.polling()
